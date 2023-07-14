82a0521418ca0ba138398ebfff381f3029041bd5
Angelique Rivera <angeliqueriveramorales@gmail.com>
2023-07-14 09:59:41 -0400

    feat(GreatFrontEnd): add folder and fromPairs file

---
+++ practice problems/GreatFrontEnd/fromPairs.js
@@ -0,0 +1,34 @@
+/**
+ * @see {@link https://www.greatfrontend.com/questions/javascript/from-pairs fromPairs}
+ */
+
+/**
+ * EXPLAIN
+ * This is the plain English explanation
+ */
+
+/**
+ * APPROACH
+ * Use code terminology here to explain steps
+ */
+
+export default function fromPairs(pairs) {
+  throw "Not implemented!";
+}
+
+/**
+ * COMPLEXITY
+ * TIME   O()
+ * SPACE  O()
+ */
+
+/**
+ * TIME TAKEN
+ *
+ * READ             0
+ * EXPLAIN          0
+ * APPROACH         0
+ * CODE [FAIL]      0
+ * TEST             0
+ * OPTIMIZE         0
+ */
